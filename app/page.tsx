2ca05b62100f28d481096b6c27933e34f8f97e5d
Tachera Sasi <tacherasasi@gmail.com>
2025-08-22 17:51:35 +0300

    feat: refactor Philosophy component to improve structure and add grid layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Testimonials from "@/components/testimonials";
 import Products from "@/components/products";
 import Faq from "@/components/faq";
 import Footer from "@/components/footer";
-import { Philosophy } from "@/components/philosophy";
+import {Philosophy} from "@/components/philosophy";
 
 export default function Home() {
   return (
